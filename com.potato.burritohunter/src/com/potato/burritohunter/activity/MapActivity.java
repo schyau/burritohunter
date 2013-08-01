c100d41db3f338f659373e93dec6c97329b715c4
Stephen Chyau <Stephen.Chyau@Blackboard.com>
2013-07-31 23:06:27 -0700

    add buttons to map, uncomment location tracking code and fix npe when location is not received, add button feature to track my current location

--- com.potato.burritohunter/src/com/potato/burritohunter/activity/MapActivity.java
+++ com.potato.burritohunter/src/com/potato/burritohunter/activity/MapActivity.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.location.Location;
@@ -13,6 +14,9 @@ import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -41,29 +45,70 @@ public class MapActivity extends FragmentActivity implements
 	private GoogleMap map;
 	private LocationClient mLocationClient;
 	Location mCurrentLocation;
-	
-	//TODO move this to a file that allows encapsulated global access
-	//TODO use a better data structure, what is better?	
+
+	// TODO move this to a file that allows encapsulated global access
+	// TODO use a better data structure, what is better?
 	private static final HashMap<Marker, SearchResult> currentSearchResults = new HashMap<Marker, SearchResult>();
-	private static final ArrayList<Marker> selectedSearchResults = new ArrayList<Marker>();  //TODO fetch from hashmap, then serialize
-		
+	private static final ArrayList<Marker> selectedSearchResults = new ArrayList<Marker>(); // TODO
+																							// fetch
+																							// from
+																							// hashmap,
+																							// then
+																							// serialize
 
 	private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
 	private static final String TAG = MapActivity.class.getName();
 
 	private LatLng PLACE = new LatLng(37.871744, -122.260963);
+	private static Marker MY_LOCATION; // TODO serialize last location and put
+										// it in here on app startup.
+
+	private Context _ctx;
+
+	private void clearMyLocationMarker() {
+		if (MY_LOCATION != null) {
+			MY_LOCATION.remove();
+		}
+	}
+
+	private void getCurrentLocation() {
+		if (mLocationClient != null) {
+			mCurrentLocation = mLocationClient.getLastLocation();
+		} else {
+		}
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
-		
-        /*
-         * Create a new location client, using the enclosing class to
-         * handle callbacks.
-         */
-        mLocationClient = new LocationClient(this, this, this);
-        
+		_ctx = this;
+		/*
+		 * Create a new location client, using the enclosing class to handle
+		 * callbacks.
+		 */
+		mLocationClient = new LocationClient(this, this, this);
+
+		Button findMe = (Button) findViewById(R.id.find_me);
+		findMe.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				getCurrentLocation();
+				if (mCurrentLocation != null) {
+					clearMyLocationMarker();
+					MY_LOCATION = map.addMarker(new MarkerOptions()
+							.position(new LatLng(
+									mCurrentLocation.getLatitude(),
+									mCurrentLocation.getLongitude())));
+				} else {
+					Toast.makeText(_ctx, "can't get current location sry",
+							Toast.LENGTH_SHORT).show();
+				}
+
+			}
+
+		});
 
 		// new YelpRequestAsyncTask(37.871744, -122.260963, "burritos",
 		// SomeUtil.getBus()).execute();
@@ -71,38 +116,24 @@ public class MapActivity extends FragmentActivity implements
 				SomeUtil.getBus()).execute();
 		map = ((SupportMapFragment) getSupportFragmentManager()
 				.findFragmentById(R.id.map)).getMap();
-		
-		//should i store the marker or the pojo?
-		map.setOnMarkerClickListener(new OnMarkerClickListener(){
 
+		// should i store the marker or the pojo?
+		map.setOnMarkerClickListener(new OnMarkerClickListener() {
 			@Override
 			public boolean onMarkerClick(Marker marker) {
 				// TODO Auto-generated method stub
-				if(selectedSearchResults.contains(marker))
-				{
+				if (selectedSearchResults.contains(marker)) {
 					marker.setIcon(BitmapDescriptorFactory
-						.fromResource(R.drawable.ic_launcher));
-					selectedSearchResults.add(marker);
-				}
-				else
-				{
-					marker.setIcon(BitmapDescriptorFactory
-						.fromResource(R.drawable.ic_launcher_clicked));
+							.fromResource(R.drawable.ic_launcher));
 					selectedSearchResults.remove(marker);
+				} else {
+					marker.setIcon(BitmapDescriptorFactory
+							.fromResource(R.drawable.ic_launcher_clicked));
+					selectedSearchResults.add(marker);
 				}
 				return false;
 			}
 		});
-		/*
-		 * Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
-		 * .title("Hamburg"));
-		 */
-		Marker kiel = map.addMarker(new MarkerOptions()
-				.position(PLACE)
-				.title("Kiel")
-				.snippet("Kiel is cool")
-				.icon(BitmapDescriptorFactory
-						.fromResource(R.drawable.ic_launcher)));
 
 		// Move the camera instantly to hamburg with a zoom of 15.
 		map.moveCamera(CameraUpdateFactory.newLatLngZoom(PLACE, 15));
@@ -111,25 +142,26 @@ public class MapActivity extends FragmentActivity implements
 		map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
 	}
 
-    /*
-     * Called when the Activity becomes visible.
-     */
-    @Override
-    protected void onStart() {
-        super.onStart();
-        // Connect the client.
-        //mLocationClient.connect();
-    }
+	/*
+	 * Called when the Activity becomes visible.
+	 */
+	@Override
+	protected void onStart() {
+		super.onStart();
+		// Connect the client.
+		mLocationClient.connect();
+	}
 
-    /*
-     * Called when the Activity is no longer visible.
-     */
-    @Override
-    protected void onStop() {
-        // Disconnecting the client invalidates it.
-        //mLocationClient.disconnect();
-        super.onStop();
-    }
+	/*
+	 * Called when the Activity is no longer visible.
+	 */
+	@Override
+	protected void onStop() {
+		// Disconnecting the client invalidates it.
+		// mLocationClient.disconnect();
+		super.onStop();
+	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -162,19 +194,19 @@ public class MapActivity extends FragmentActivity implements
 
 	@Subscribe
 	public void subscriberWithASillyName(ArrayList<SearchResult> searcResults) {
-		//Log.d("luls", y.getStatus());
-		for ( SearchResult s : searcResults )
-		{
+		// Log.d("luls", y.getStatus());
+		for (SearchResult s : searcResults) {
 			// TODO make a big ass Marker class with its own onclicklistener
 			Marker marker = map.addMarker(new MarkerOptions()
-			.position(s._latlng)
-			.title(s._name)
-			.snippet("Kiel is cool")
-			.icon(BitmapDescriptorFactory
-					.fromResource(R.drawable.ic_launcher)));
+					.position(s._latlng)
+					.title(s._name)
+					.snippet("Kiel is cool")
+					.icon(BitmapDescriptorFactory
+							.fromResource(R.drawable.ic_launcher)));
 			currentSearchResults.put(marker, s);
 		}
 	}
+
 	// Define a DialogFragment that displays the error dialog
 	public static class ErrorDialogFragment extends DialogFragment {
 		// Global field to contain the error dialog
@@ -232,7 +264,7 @@ public class MapActivity extends FragmentActivity implements
 			// Google Play services was not available for some reason
 		} else {
 			// Get the error code
-			//int errorCode = connectionResult.getErrorCode(); wtf man?
+			// int errorCode = connectionResult.getErrorCode(); wtf man?
 			// Get the error dialog from Google Play services
 			Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(
 					resultCode, this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
@@ -253,56 +285,59 @@ public class MapActivity extends FragmentActivity implements
 
 	@Override
 	public void onConnectionFailed(ConnectionResult connectionResult) {
-        /*
-         * Google Play services can resolve some errors it detects.
-         * If the error has a resolution, try sending an Intent to
-         * start a Google Play services activity that can resolve
-         * error.
-         */
-        if (connectionResult.hasResolution()) {
-            try {
-                // Start an Activity that tries to resolve the error
-                connectionResult.startResolutionForResult(
-                        this,
-                        CONNECTION_FAILURE_RESOLUTION_REQUEST);
-                /*
-                 * Thrown if Google Play services canceled the original
-                 * PendingIntent
-                 */
-            } catch (IntentSender.SendIntentException e) {
-                // Log the error
-                e.printStackTrace();
-            }
-        } else {
-            /*
-             * If no resolution is available, display a dialog to the
-             * user with the error.
-             */
-			//showErrorDialog(connectionResult.getErrorCode());
-            Toast.makeText(this, "Connection Failed!", Toast.LENGTH_SHORT).show();
-        }
+		/*
+		 * Google Play services can resolve some errors it detects. If the error
+		 * has a resolution, try sending an Intent to start a Google Play
+		 * services activity that can resolve error.
+		 */
+		if (connectionResult.hasResolution()) {
+			try {
+				// Start an Activity that tries to resolve the error
+				connectionResult.startResolutionForResult(this,
+						CONNECTION_FAILURE_RESOLUTION_REQUEST);
+				/*
+				 * Thrown if Google Play services canceled the original
+				 * PendingIntent
+				 */
+			} catch (IntentSender.SendIntentException e) {
+				// Log the error
+				e.printStackTrace();
+			}
+		} else {
+			/*
+			 * If no resolution is available, display a dialog to the user with
+			 * the error.
+			 */
+			// showErrorDialog(connectionResult.getErrorCode());
+			Toast.makeText(this, "Connection Failed!", Toast.LENGTH_SHORT)
+					.show();
+		}
 	}
 
 	@Override
 	public void onConnected(Bundle connectionHint) {
-        // Display the connection status
-        Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
-        mCurrentLocation = mLocationClient.getLastLocation();
-		Marker kiel = map.addMarker(new MarkerOptions()
-		.position(new LatLng(mCurrentLocation.getLatitude(),mCurrentLocation.getLongitude()))
-		.title("Kiel")
-		.snippet("Kiel is cool")
-		.icon(BitmapDescriptorFactory
-				.fromResource(R.drawable.ic_launcher)));
-        
+		// Display the connection status
+		Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+		mCurrentLocation = mLocationClient.getLastLocation();
+		if (mCurrentLocation != null) {
+			MY_LOCATION = map.addMarker(new MarkerOptions()
+					.position(
+							new LatLng(mCurrentLocation.getLatitude(),
+									mCurrentLocation.getLongitude()))
+					.title("Kiel")
+					.snippet("Kiel is cool")
+					.icon(BitmapDescriptorFactory
+							.fromResource(R.drawable.ic_launcher)));
+		}
+
 	}
 
 	@Override
 	public void onDisconnected() {
 		// Display the connection status
-        Toast.makeText(this, "Disconnected. Please re-connect.",
-                Toast.LENGTH_SHORT).show();
-		
+		Toast.makeText(this, "Disconnected. Please re-connect.",
+				Toast.LENGTH_SHORT).show();
+
 	}
 
 }
